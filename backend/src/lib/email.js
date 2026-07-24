c607b4aa16cdcbb4d8b250769a2b412cbbc216ab
tabish-0 <tabish7raj@gmail.com>
2026-07-24 17:03:28 +0530

    done

--- backend/src/lib/email.js
+++ backend/src/lib/email.js
@@ -1,4 +1,4 @@
-import SibApiV3Sdk from "@getbrevo/brevo";
+import * as SibApiV3Sdk from "@getbrevo/brevo";
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
